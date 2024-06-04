0bd3c59b9c45e7a8bf49ddb610e9d1e60519dcb7
sanchayvashist <s.vast979@gmail.com>
2024-06-04 18:11:31 +0530

    Get ATS score from resume and job description

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ from PIL import Image
 from dotenv import load_dotenv
 
 
+# Get response from gemini 
 def get_gemini_response(model, resume_text, job_description):
     message = []
 
-    print("Start Gen AI")
+    print("Generating ATS score using AI")
 
     replacements = {
         "resume_text" : resume_text,
@@ -31,6 +32,7 @@ def get_gemini_response(model, resume_text, job_description):
     response = model.generate_content(message)
     return response.text
 
+# Funciton to Remove Rotation from image
 def straighen_image(image):
     text = pytesseract.image_to_osd(image)
 
@@ -47,43 +49,26 @@ if __name__ == "__main__":
     load_dotenv()
     reader = easyocr.Reader(['en'])
 
+    # Convert pdf to image
     image_list = convert_from_path("Sanchay.pdf")
+
+    # Get api key from enviroment and intialize model
     key = os.environ["API_KEY"]
     genai.configure(api_key=key)
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel('gemini-1.5-pro')
 
+    # Use OCR to extract text from image
+    resume_text = ""
     for image in image_list:
         image = straighen_image(image)
         data = np.array(image)
         results = reader.readtext(data, paragraph=True)
-        resume_text = ""
         for result in results:
             resume_text = resume_text + " " + result[1]
     
-    job_description = '''Key Responsibilities And Accountabilities
-    Design, build, and measure complex ELT jobs to process disparate data sources and form a high integrity, high quality, clean data asset.
-    Working on a range of projects including batch pipelines, data modeling, and data mart solutions you’ll be part of collaborative project teams working to implement robust data collection and processing pipelines to meet specific business need.
-    Goals
-
-    Design, build, and measure complex ELT jobs to process disparate data sources and form a high integrity, high quality, clean data asset.
-    Executes and provides feedback for data modeling policies, procedure, processes, and standards.
-    Assists with capturing and documenting system flow and other pertinent technical information about data, database design, and systems.
-    Develop data quality standards and tools for ensuring accuracy.
-    Work across departments to understand new data patterns.
-    Translate high-level business requirements into technical specs.
-
-    Required
-
-    Bachelor’s degree in computer science or engineering.
-    5+ years of experience with data analytics, data modeling, and database design.
-    3+ years of coding and scripting (Python, Java, Pyspark) and design experience.
-    3+ years of experience with Spark framework.
-    Experience with ELT methodologies and tools.
-    Experience with Vertica OR Teradata
-    Expertise in tuning and troubleshooting SQL.
-    Strong data integrity, analytical and multitasking skills.
-    Excellent communication, problem solving, organizational and analytical skills.
-    Able to work independently.'''
+    # Get job description from text file 
+    with open("job_desrciption.txt", "r") as file:
+        job_description = file.read()
 
     print(get_gemini_response(model, resume_text, job_description))
         
